3625c9693242af528f0886d211e657dd00b4ad33
vinaybetabulls <bodigevinay92@gmail.com>
2020-08-18 20:42:32 +0530

    api health check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiTags('Health Check')
   @Get()
+  @ApiOperation({summary: 'Api Health Check'})
   getHello(): string {
     return this.appService.getHello();
   }
