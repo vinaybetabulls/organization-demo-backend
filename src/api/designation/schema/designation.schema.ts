4125ddd19cce6f74c028a7ca19dbf371fecad7db
vinaybetabulls <bodigevinay92@gmail.com>
2020-08-20 09:55:34 +0530

    updated designationId in schema

--- src/api/designation/schema/designation.schema.ts
+++ src/api/designation/schema/designation.schema.ts
@@ -2,7 +2,7 @@ import * as mongoose from 'mongoose';
 
 export const DesignationSchema = new mongoose.Schema({
     designationtName : {type: String},
-    designationtId: {type: String},
+    designationId: {type: String},
     createdBy: {
         userId: {type: String},
         email: {type: String}
